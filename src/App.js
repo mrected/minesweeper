19a04e31acba23766f0fc5fbf8f4424c6c2b2065
Chris Terrazas <chris.terrazas@gmail.com>
2018-11-23 16:21:52 -0500

    connected to api via axios

--- src/App.js
+++ src/App.js
@@ -39,6 +39,24 @@ class App extends Component {
     })
   }
 
+  checkCell = (row, col) => {
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com${this.state.game.id}/check`,
+        {
+          id: this.state.game,
+          row: row,
+          col: col
+        }
+      )
+      .then(response => {
+        this.setState({
+          playing: true,
+          game: response.data
+        })
+      })
+  }
+
   checkPlaying = () => {
     if (!this.state.playing) {
       return 'Start a new game!'
@@ -79,84 +97,90 @@ class App extends Component {
               </td>
             </tr>
             <tr>
-              <Cell value={this.state.game.board[0][0]} />
-              <Cell value={this.state.game.board[0][1]} />
-              <Cell value={this.state.game.board[0][2]} />
-              <Cell value={this.state.game.board[0][3]} />
-              <Cell value={this.state.game.board[0][4]} />
-              <Cell value={this.state.game.board[0][5]} />
-              <Cell value={this.state.game.board[0][6]} />
-              <Cell value={this.state.game.board[0][7]} />
+              <Cell
+                row={0}
+                col={0}
+                row={0}
+                col={0}
+                value={this.state.game.board[0][0]}
+              />
+              <Cell row={0} col={1} value={this.state.game.board[0][1]} />
+              <Cell row={0} col={2} value={this.state.game.board[0][2]} />
+              <Cell row={0} col={3} value={this.state.game.board[0][3]} />
+              <Cell row={0} col={4} value={this.state.game.board[0][4]} />
+              <Cell row={0} col={5} value={this.state.game.board[0][5]} />
+              <Cell row={0} col={6} value={this.state.game.board[0][6]} />
+              <Cell row={0} col={7} value={this.state.game.board[0][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[1][0]} />
-              <Cell value={this.state.game.board[1][1]} />
-              <Cell value={this.state.game.board[1][2]} />
-              <Cell value={this.state.game.board[1][3]} />
-              <Cell value={this.state.game.board[1][4]} />
-              <Cell value={this.state.game.board[1][5]} />
-              <Cell value={this.state.game.board[1][6]} />
-              <Cell value={this.state.game.board[1][7]} />
+              <Cell row={1} col={0} value={this.state.game.board[1][0]} />
+              <Cell row={1} col={1} value={this.state.game.board[1][1]} />
+              <Cell row={1} col={2} value={this.state.game.board[1][2]} />
+              <Cell row={1} col={3} value={this.state.game.board[1][3]} />
+              <Cell row={1} col={4} value={this.state.game.board[1][4]} />
+              <Cell row={1} col={5} value={this.state.game.board[1][5]} />
+              <Cell row={1} col={6} value={this.state.game.board[1][6]} />
+              <Cell row={1} col={7} value={this.state.game.board[1][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[2][0]} />
-              <Cell value={this.state.game.board[2][1]} />
-              <Cell value={this.state.game.board[2][2]} />
-              <Cell value={this.state.game.board[2][3]} />
-              <Cell value={this.state.game.board[2][4]} />
-              <Cell value={this.state.game.board[2][5]} />
-              <Cell value={this.state.game.board[2][6]} />
-              <Cell value={this.state.game.board[2][7]} />
+              <Cell row={2} col={0} value={this.state.game.board[2][0]} />
+              <Cell row={2} col={1} value={this.state.game.board[2][1]} />
+              <Cell row={2} col={2} value={this.state.game.board[2][2]} />
+              <Cell row={2} col={3} value={this.state.game.board[2][3]} />
+              <Cell row={2} col={4} value={this.state.game.board[2][4]} />
+              <Cell row={2} col={5} value={this.state.game.board[2][5]} />
+              <Cell row={2} col={6} value={this.state.game.board[2][6]} />
+              <Cell row={2} col={7} value={this.state.game.board[2][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[3][0]} />
-              <Cell value={this.state.game.board[3][1]} />
-              <Cell value={this.state.game.board[3][2]} />
-              <Cell value={this.state.game.board[3][3]} />
-              <Cell value={this.state.game.board[3][4]} />
-              <Cell value={this.state.game.board[3][5]} />
-              <Cell value={this.state.game.board[3][6]} />
-              <Cell value={this.state.game.board[3][7]} />
+              <Cell row={3} col={0} value={this.state.game.board[3][0]} />
+              <Cell row={3} col={1} value={this.state.game.board[3][1]} />
+              <Cell row={3} col={2} value={this.state.game.board[3][2]} />
+              <Cell row={3} col={3} value={this.state.game.board[3][3]} />
+              <Cell row={3} col={4} value={this.state.game.board[3][4]} />
+              <Cell row={3} col={5} value={this.state.game.board[3][5]} />
+              <Cell row={3} col={6} value={this.state.game.board[3][6]} />
+              <Cell row={3} col={7} value={this.state.game.board[3][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[4][0]} />
-              <Cell value={this.state.game.board[4][1]} />
-              <Cell value={this.state.game.board[4][2]} />
-              <Cell value={this.state.game.board[4][3]} />
-              <Cell value={this.state.game.board[4][4]} />
-              <Cell value={this.state.game.board[4][5]} />
-              <Cell value={this.state.game.board[4][6]} />
-              <Cell value={this.state.game.board[4][7]} />
+              <Cell row={4} col={0} value={this.state.game.board[4][0]} />
+              <Cell row={4} col={1} value={this.state.game.board[4][1]} />
+              <Cell row={4} col={2} value={this.state.game.board[4][2]} />
+              <Cell row={4} col={3} value={this.state.game.board[4][3]} />
+              <Cell row={4} col={4} value={this.state.game.board[4][4]} />
+              <Cell row={4} col={5} value={this.state.game.board[4][5]} />
+              <Cell row={4} col={6} value={this.state.game.board[4][6]} />
+              <Cell row={4} col={7} value={this.state.game.board[4][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[5][0]} />
-              <Cell value={this.state.game.board[5][1]} />
-              <Cell value={this.state.game.board[5][2]} />
-              <Cell value={this.state.game.board[5][3]} />
-              <Cell value={this.state.game.board[5][4]} />
-              <Cell value={this.state.game.board[5][5]} />
-              <Cell value={this.state.game.board[5][6]} />
-              <Cell value={this.state.game.board[5][7]} />
+              <Cell row={5} col={0} value={this.state.game.board[5][0]} />
+              <Cell row={5} col={1} value={this.state.game.board[5][1]} />
+              <Cell row={5} col={2} value={this.state.game.board[5][2]} />
+              <Cell row={5} col={3} value={this.state.game.board[5][3]} />
+              <Cell row={5} col={4} value={this.state.game.board[5][4]} />
+              <Cell row={5} col={5} value={this.state.game.board[5][5]} />
+              <Cell row={5} col={6} value={this.state.game.board[5][6]} />
+              <Cell row={5} col={7} value={this.state.game.board[5][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[6][0]} />
-              <Cell value={this.state.game.board[6][1]} />
-              <Cell value={this.state.game.board[6][2]} />
-              <Cell value={this.state.game.board[6][3]} />
-              <Cell value={this.state.game.board[6][4]} />
-              <Cell value={this.state.game.board[6][5]} />
-              <Cell value={this.state.game.board[6][6]} />
-              <Cell value={this.state.game.board[6][7]} />
+              <Cell row={6} col={0} value={this.state.game.board[6][0]} />
+              <Cell row={6} col={1} value={this.state.game.board[6][1]} />
+              <Cell row={6} col={2} value={this.state.game.board[6][2]} />
+              <Cell row={6} col={3} value={this.state.game.board[6][3]} />
+              <Cell row={6} col={4} value={this.state.game.board[6][4]} />
+              <Cell row={6} col={5} value={this.state.game.board[6][5]} />
+              <Cell row={6} col={6} value={this.state.game.board[6][6]} />
+              <Cell row={6} col={7} value={this.state.game.board[6][7]} />
             </tr>
             <tr>
-              <Cell value={this.state.game.board[7][0]} />
-              <Cell value={this.state.game.board[7][1]} />
-              <Cell value={this.state.game.board[7][2]} />
-              <Cell value={this.state.game.board[7][3]} />
-              <Cell value={this.state.game.board[7][4]} />
-              <Cell value={this.state.game.board[7][5]} />
-              <Cell value={this.state.game.board[7][6]} />
-              <Cell value={this.state.game.board[7][7]} />
+              <Cell row={7} col={0} value={this.state.game.board[7][0]} />
+              <Cell row={7} col={1} value={this.state.game.board[7][1]} />
+              <Cell row={7} col={2} value={this.state.game.board[7][2]} />
+              <Cell row={7} col={3} value={this.state.game.board[7][3]} />
+              <Cell row={7} col={4} value={this.state.game.board[7][4]} />
+              <Cell row={7} col={5} value={this.state.game.board[7][5]} />
+              <Cell row={7} col={6} value={this.state.game.board[7][6]} />
+              <Cell row={7} col={7} value={this.state.game.board[7][7]} />
             </tr>
             <tr>
               <td className="header" colSpan="8">
--- src/Cell.js
+++ src/Cell.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Cell extends Component {
+  flagCell = event => {
+    console.log(
+      `this was clicked at row ${this.props.row} and col ${this.props.col}`
+    )
+  }
 
-    flagCell = (event) => {
-        console.log(event)
-    }
-
-    render() {
-        return (
-            <td onClick={this.flagCell}>
-                {this.props.value}
-            </td>
-        );
-    }
+  render() {
+    return <td onClick={this.flagCell}>{this.props.value}</td>
+  }
 }
 
-export default Cell;
+export default Cell
